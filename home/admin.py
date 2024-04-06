ed0d4f52f2b8ba2126397fe8a1ccc3ff3745885a
Rohit T P <tprohit9@gmail.com>
2024-04-06 21:57:11 +0530

    ui done

--- home/admin.py
+++ home/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+
+from home.models import Profile
+
+admin.site.register(Profile)
--- home/models.py
+++ home/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='profile')
+    picture_url = models.URLField()
